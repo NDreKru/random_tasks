44b5162e61bdf6a1fd1cddcb08377e6cccbb7805
NDreKru <ndrekrukru@gmail.com>
2021-07-15 15:11:04 +0300

    6kyu/Count the number of Duplicates

---
+++ src/codewars/CountingDuplicates.java
@@ -0,0 +1,34 @@
+package codewars;
+
+/*Count the number of Duplicates
+
+        Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+        Example
+
+        "abcde" -> 0 # no characters repeats more than once
+        "aabbcde" -> 2 # 'a' and 'b'
+        "aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+        "indivisibility" -> 1 # 'i' occurs six times
+        "Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
+        "aA11" -> 2 # 'a' and '1'
+        "ABBA" -> 2 # 'A' and 'B' each occur twice*/
+
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class CountingDuplicates {
+    public static int duplicateCount(String text) {
+        int count = 0;
+        List<Character> charList = text.toLowerCase(Locale.ROOT)
+                .chars()
+                .mapToObj(c -> (char) c)
+                .collect(Collectors.toList());
+        Map<Character, Integer> map = charList.stream()
+                .collect(Collectors.toMap(character -> character, v -> count, (num1, num2) -> 1));
+        return map.values()
+                .stream()
+                .reduce(0, Integer::sum);
+    }
+}
